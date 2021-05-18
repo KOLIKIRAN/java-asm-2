22795ed6b40f67811d27406a03ba8a58283e2daf
KOLIKIRAN <83825273+KOLIKIRAN@users.noreply.github.com>
2021-05-18 09:58:09 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+public class pattern1{
+public static void main(String args[])
+{
+int a=0;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+
+System.out.print((int)(a+j)+" ");
+
+
+}
+System.out.println();
+}
+}
+}
---
+++ pattern11.java
@@ -0,0 +1,16 @@
+class pattern11{
+public static void main(String rgs[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=5;j>=1;j--)
+{
+if(j>i)
+System.out.print(" ");
+else
+System.out.print(" *");
+}
+System.out.println();
+}
+}
+}
